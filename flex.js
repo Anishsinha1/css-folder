345afaa589bbcc24338042dd009fb3d8b5a6f47a
Anish sinha <124868577+Anishsinha1@users.noreply.github.com>
2023-11-08 10:05:01 +0530

    gy

---
+++ flex.js
@@ -0,0 +1,28 @@
+
+    document.getElementById("btn1").addEventListener("click",function(){
+        document.getElementById("body").style.backgroundColor="pink";
+        document.getElementById("btn1").style.border="4px solid black";
+        document.getElementById("btn1").style.fontSize="28px";
+
+    });
+    
+    document.getElementById("btn2").addEventListener("click",function(){
+        document.getElementById("body").style.backgroundColor="red";
+        document.getElementById("btn2").style.border="4px solid black";
+        document.getElementById("btn2").style.fontSize="28px";
+    });
+    document.getElementById("btn3").addEventListener("click",function(){
+        document.getElementById("body").style.backgroundColor="green";
+        document.getElementById("btn3").style.border="4px solid black";
+        document.getElementById("btn3").style.fontSize="28px";
+    });
+    document.getElementById("btn4").addEventListener("click",function(){
+        document.getElementById("body").style.backgroundColor="yellow";
+        document.getElementById("btn4").style.border="4px solid black";
+        document.getElementById("btn4").style.fontSize="28px";
+    });
+    document.getElementById("btn5").addEventListener("click",function(){
+        document.getElementById("body").style.backgroundColor="orange";
+        document.getElementById("btn5").style.border="4px solid black";
+        document.getElementById("btn5").style.fontSize="28px";
+    });
